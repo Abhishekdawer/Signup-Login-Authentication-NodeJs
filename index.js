db378389272316b8ae0f62595fc08d20b61893fb
Abhishekdawer <abhishekdawer045@gmail.com>
2020-06-13 17:40:20 +0530

    second commit

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.set('views',path.join(__dirname,'views'));
 app.get('/',routes);
 app.post('/register',routes);
 app.get('/login',routes);
+app.post('/login',routes);
+app.get('/success',routes);
+app.get('/logout',routes);
+app.post('/addmsg',routes);
 
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
--- routes.js
+++ routes.js
@@ -4,8 +4,44 @@ const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const user = require('./models/usermodel.js');
+const passport = require('passport');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
 
 routes.use(bodyparser.urlencoded({ extended:true}));
+routes.use(cookieParser('secret'));
+routes.use(session({
+    secret: 'secret',
+    maxAge : 3600000,
+    resave : true ,
+    saveUninitialized : true,
+}));
+
+routes.use(passport.initialize());
+routes.use(passport.session());
+routes.use(flash());
+
+routes.use(function(req,res,next){
+res.locals.success_message = req.flash('success_message');
+res.locals.error_message = req.flash('error_message');
+res.locals.error = req.flash('error');
+next();
+});
+
+const checkAuthenticated = function(req,res,next)
+{
+    if(req.isAuthenticated())
+    {
+        res.set('Cache-control, private, no-store, must-revalidate, post-check =0, pre-check=0');
+        return next();
+    }
+    else
+    {
+        res.redirect('/login');
+    }
+}
 
 mongoose.connect('mongodb://localhost:27017/userDB',{
     useNewUrlParser:true, useUnifiedTopology:true,
@@ -57,7 +93,76 @@ routes.post('/register',(req,res)=>{
 
 });
 
+var localStrategy = require('passport-local').Strategy;
+passport.use(new localStrategy({usernameField: 'email'}, (email , password , done)=>{
+    user.findOne({email: email}, (err,data)=>{
+        if(err) throw err;
+        if(!data)
+        {
+            return done(null,false,{ message: "User Doesn't Exists.." });
+        }
+        bcrypt.compare(password,data.password,(err,match)=>{
+            if(err)
+            {
+                return done(null,false);
+            }
+            if(!match)
+            {
+                return done(null,false, { message: "Password Doesn't Match" });
+            }
+            if(match)
+            {
+                return done(null,data);
+            }
+        });
+    });
+}));
+
+
+passport.serializeUser(function(user,cb){
+    cb(null,user.id);
+});
+
+passport.deserializeUser(function(id , cb){
+    user.findById(id, function(err, user){
+        cb(err,user);
+    });
+});
+
 routes.get('/login',(req,res)=>{
  res.render('login');
 });
+
+routes.post('/login',(req,res,next)=>{
+passport.authenticate('local',{
+    failureRedirect: '/login',
+    successRedirect : '/success',
+    failureFlash : true,
+})(req,res,next);
+});
+
+routes.get('/success',checkAuthenticated,(req,res)=>{
+res.render('success', { 'user': req.user });
+});
+
+routes.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/login');
+});
+
+routes.post('/addmsg', checkAuthenticated, (req, res) => {
+    user.findOneAndUpdate(
+        { email: req.user.email },
+        {
+            $push: {
+                messages: req.body['msg']
+            }
+        }, (err, suc) => {
+            if (err) throw err;
+            if (suc) console.log("Added Successfully...");
+        }
+    );
+    res.redirect('/success');
+}); 
+
 module.exports = routes;
